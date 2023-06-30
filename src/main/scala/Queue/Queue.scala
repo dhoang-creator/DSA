d5dc2e08403fa0e99da681dd4855c1a804cff8c0
Duy Hoang <duyhoang123@hotmail.co.uk>
2023-06-30 19:20:54 +0200

    attempt at a queue

--- src/main/scala/Queue/Queue.scala
+++ src/main/scala/Queue/Queue.scala
@@ -1,14 +1,16 @@
 package Queue
 
 sealed trait Queue[+A]
-case object Nil extends Queue[Nothing]
-case class Cons[+A](head: A, tail: Queue[A]) extends Queue[A]
+case class Node[A](value: A) extends Queue[A]
+case class Cons[A](head: A, tail: Queue[A]) extends Queue[A]
 
 // Is the structure of a Queue simply the same as LinkedList but with different constraints?
 object Queue {
+  val queue: Queue[Int] = Node(0)
 
-  // Remember that the '*' indicates varargs
-  def apply[A](as: A*): Queue[A] =
-    if (as.isEmpty) Nil
-    else Cons(as.head, apply(as.tail: _*))
+  // Aren't we just rewriting the Tree data structure into a queue
+  def asList[T](queue: Queue[T]): List[T] = queue match {
+    case Node(a) => List(a)
+    case Cons(head, tail) =>
+  }
 }
