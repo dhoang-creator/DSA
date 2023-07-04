c586c22b4e3393b7a46920803b614d748374df10
Duy Hoang <duyhoang123@hotmail.co.uk>
2023-07-04 07:32:00 +0200

    double linked list started

--- src/main/scala/LinkedLists/SingleList.scala
+++ src/main/scala/LinkedLists/SingleList.scala
@@ -1,23 +1,23 @@
 package LinkedLists
 
-sealed trait List[+A]
-case object Nil extends List[Nothing]
-case class Cons[+A](head: A, tail: List[A]) extends List[A]
+sealed trait SingleList[+A]
+case object Nil extends SingleList[Nothing]
+case class Cons[+A](head: A, tail: SingleList[A]) extends SingleList[A]
 
-object List {
-  def sum(ints: List[Int]): Int = ints match {
+object SingleList {
+  def sum(ints: SingleList[Int]): Int = ints match {
     case Nil => 0
     case Cons(x, xs) => x + sum(xs)
   }
 
-  def product(ds: List[Double]): Double = ds match {
+  def product(ds: SingleList[Double]): Double = ds match {
     case Nil => 1.0
     case Cons(0.0, _) => 0.0
     case Cons(x, xs) => x * product(xs)
   }
 
   // Notice that the asterix in the method signature below is indicative of a variadic argument
-  def apply[A](as: A*): List[A] =
+  def apply[A](as: A*): SingleList[A] =
     if (as.isEmpty) Nil
     else Cons(as.head, apply(as.tail: _*))
 }
