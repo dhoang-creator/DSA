9bb11427a0cabdc010c9feb11fb98e66ff923d03
Duy Hoang <duyhoang123@hotmail.co.uk>
2023-07-04 07:24:05 +0200

    linked lists added

---
+++ src/main/scala/LinkedLists/List.scala
@@ -0,0 +1,23 @@
+package LinkedLists
+
+sealed trait List[+A]
+case object Nil extends List[Nothing]
+case class Cons[+A](head: A, tail: List[A]) extends List[A]
+
+object List {
+  def sum(ints: List[Int]): Int = ints match {
+    case Nil => 0
+    case Cons(x, xs) => x + sum(xs)
+  }
+
+  def product(ds: List[Double]): Double = ds match {
+    case Nil => 1.0
+    case Cons(0.0, _) => 0.0
+    case Cons(x, xs) => x * product(xs)
+  }
+
+  // Notice that the asterix in the method signature below is indicative of a variadic argument
+  def apply[A](as: A*): List[A] =
+    if (as.isEmpty) Nil
+    else Cons(as.head, apply(as.tail: _*))
+}
